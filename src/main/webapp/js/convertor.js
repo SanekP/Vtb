10e6a24d9a77cab9175f434f6013e67bb9923403
SanekP <sanekne@mail.ru>
2014-09-05 22:03:24 +0300

    Add converter interface to page

---
+++ src/main/webapp/js/convertor.js
@@ -0,0 +1,21 @@
+$(function() {
+	var convertor = $("#convertor");
+	var button = $('input[type="button"]', convertor);
+
+	function success(data) {
+		if (data.result) {
+			$('input[name="result"]', convertor).val(data.result);
+		}
+		button.fadeIn();
+	}
+
+	function convert() {
+		button.fadeOut();
+		$.post("convert", convertor.serialize(), success).fail(function() {
+			alert("Something went wrong :(");
+			button.slideDown();
+		});
+	}
+
+	button.click(convert);
+});
